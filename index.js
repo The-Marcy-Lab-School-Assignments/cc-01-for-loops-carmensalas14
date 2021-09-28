8153d70ff157437cff825e5d74263005e48d2335
carmen <carmensalas0114@gmail.com>
2021-09-28 14:45:40 +0000

    completed two problems

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-//1
-function countToFifty(){
-  
+/* 
+for([intial expression] [contdition expression][increment expression]){
+    statement 
+}
+i =0
+i = 9 
+9
+i=10
+10 
+i=11
+for(let i = 0; i < 10; i++){
+    console.log(i)
 }
 
+*/
+//1
+function countToFifty(){
+  for(let i = 1; i <= 50; i++){
+      console.log(i)
+  }
+}
+// countToFifty()
+
+
 //2
-function countFromOne(){
+function countFromOne(number){
+    for(let i = 1; i <= number; i++){
+        console.log(i)
+    }
   
 }
+countFromOne(5)
 
 //3
 function threeAndFiveInFifty(){
